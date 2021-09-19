6222cfcd2231ecb315b4abd5a2df7ffbef6cae4a
LiuYuChing-hub <62605429+LiuYuChing-hub@users.noreply.github.com>
2021-09-19 22:51:25 +0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,24 @@
+document.write( '<nav class="navbar navbar-expand-lg navbar-light bg-light">' );
+document.write( ' <div class="container-fluid">' );
+document.write( ' <a class="navbar-brand" href="#">YuChing</a>' );
+document.write( ' <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' );
+document.write( ' <span class="navbar-toggler-icon"></span>' );
+document.write( ' </button>' );
+document.write( ' <div class="collapse navbar-collapse" id="navbarSupportedContent">' );
+document.write( ' <ul class="navbar-nav me-auto mb-2 mb-lg-0">' );
+document.write( ' <li class="nav-item">' );
+document.write( ' <a class="nav-link active" aria-current="page" href="#">Home</a>' );
+document.write( ' </li>' );
+document.write( ' <li class="nav-item">' );
+document.write( ' <a class="nav-link" href="#">Link</a>' );
+document.write( ' </li>' );
+document.write( ' </ul>' );
+// document.write( ' <form class="form-inline my-2 my-lg-0">' );
+// document.write( ' <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">' );
+// document.write( ' <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>' );
+// document.write( ' </form>' );
+document.write( '  </div>' );
+document.write( ' </div>' );
+document.write( ' </nav>' );
+
+
