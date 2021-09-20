c13310482b7de8da40e08f7787769adb46909956
LiuYuChing-hub <62605429+LiuYuChing-hub@users.noreply.github.com>
2021-09-21 00:54:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 //alert("hello");
+const idfb=document.getElementById("idfb");
+const idig=document.getElementById("idig");
+idfb.addEventListener('mouseenter',e=>{
+    idfb.classList.remove('col-sm-2');
+    idfb.classList.add('col-sm-3');
+});
+idfb.addEventListener('mouseout',e=>{
+    idfb.classList.remove('col-sm-3');
+    idfb.classList.add('col-sm-2');
+});
+
+idig.addEventListener('mouseenter',e=>{
+    idig.classList.remove('col-sm-2');
+    idig.classList.add('col-sm-3');
+});
+idig.addEventListener('mouseout',e=>{
+    idig.classList.remove('col-sm-3');
+    idig.classList.add('col-sm-2');
+});
+idfb.addEventListener("click",function(){
+    window.open("https://www.facebook.com/profile.php?id=100000129657673");
+});
+idig.addEventListener("click",function(){
+    window.open("https://www.instagram.com/claire875/");
+});
