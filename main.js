348b37cb74013f0d4b31a422be072903c4941caf
LiuYuChing-hub <62605429+LiuYuChing-hub@users.noreply.github.com>
2021-09-21 11:18:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 //alert("hello");
 const idfb=document.getElementById("idfb");
 const idig=document.getElementById("idig");
+
+const ques=document.getElementById("ques");
+const idname=document.getElementById("idname");
+const btn=document.getElementById("btn");
+const info=document.getElementById("info");
+
+const url="http://ipinfo.io/json?token=96c23e8b1c9f73"
+
 idfb.addEventListener('mouseenter',e=>{
     idfb.classList.remove('col-sm-2');
     idfb.classList.add('col-sm-3');
@@ -23,4 +31,25 @@ idfb.addEventListener("click",function(){
 });
 idig.addEventListener("click",function(){
     window.open("https://www.instagram.com/claire875/");
+});
+
+btn.addEventListener("click",function(){
+    if(idname==""||idname==null){
+        info.innerHTML="Empty!"
+    }else{
+        info.innerHTML="Hello!"+idname.value;
+        idname.style.display="none";
+        btn.style.display="none";
+        ques.style.display="none";
+        axios
+            .get(url)
+            .then(function(res){
+                info.innerHTML+='<br>IP:'+res.data.ip;
+                info.innerHTML+='<br>City:'+res.data.city;
+                info.innerHTML+='<br>Country:'+res.data.country;
+            })
+            .catch(function(err){
+                info.innerHTML+='<br>'+err;
+            });
+    }
 });
